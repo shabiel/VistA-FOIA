dfe476a9cf27a1f532b5f56d806c26f3c34dfcd0
Catalin Branea <cbranea@gmail.com>
2012-02-27 14:16:15 +0200

    Problem List Error provider A key feature of the API is to report detailed error information to the calling process. The GMPLAPIE routine will provide such functionality in the way FileMan API calls do.

---
+++ Packages/Problem List/Routines/GMPLAPIE.m
@@ -0,0 +1,30 @@
+GMPLAPIE ; / Problem List Error provider;02/27/2012
+ ;;TBD;Problem List;;02/27/2012
+ERR(ERT,ERRNO,TEXT)
+ Q:$G(ERRNO)=""
+ N I
+ S TEXT=$G(TEXT)
+ D BUILD
+ S:$G(ERT)="" ERT="^TMP(""GMPLERR"",$J)"
+ S I=+$G(@ERT)+1
+ S @ERT=I
+ S @ERT@(I)=ERRNO_"^"_$G(GMPLERR(ERRNO))_TEXT
+ Q
+ ;
+BUILD
+ Q:$D(GMPLERR)
+ N I,LINE,ERR
+ F I=1:1 S LINE=$T(ERRTABLE+I) Q:LINE=""  D
+ . S ERR=$P(LINE,";;",2)
+ . S GMPLERR($P(ERR,"^",1))=$P(ERR,"^",2)
+ Q
+ ;
+ERRTABLE
+ ;;INVALIDPARAM^Invalid parameter value - 
+ ;;FILELOCKED^"Record in use. Try again in a few moments.
+ ;;PRBNOTFOUND^Problem not found
+ ;;PRBDELETED^Problem already deleted
+ ;;PROVNOTFOUND^Provider not found
+ ;;FACNOTFOUND^Facility not found
+ ;;PRBVERIFIED^Problem Already Verified
+ 
