9afeab7e2b0687b75ca44e974d1d3b2ed75103e6
Catalin Branea <cbranea@gmail.com>
2012-03-22 09:05:19 +0200

    Replaced use of ^AUPNPROB with calls to GMPLAPI4

--- Packages/Order Entry Results Reporting/Routines/ORQQPL3.m
+++ Packages/Order Entry Results Reporting/Routines/ORQQPL3.m
@@ -28,61 +28,10 @@ LIST(GMPL,GMPDFN,GMPSTAT)       ; -- Returns list of problems for patient GMPDFN
  ; This is virtually same as LIST^GMPLUTL2 except that it appends the
  ; condition - T)ranscribed or P)ermanent,location,loc type,provider, service.
  ;
- N I,IFN,CNT,GMPL0,GMPL1,SP,ST,NUM,ONSET,ICD,LASTMOD,PRIO,DTREC
- N SC,ORLIST,ORVIEW,GMPARAM,ORTOTAL,LIN,LOC,LT,PROV,SERV,HASCMT
- N SCCOND,AO,IR,ENV,HNC,MST,CV,SHD,ORICD186,INACT
+ N GMPREV
  Q:$G(GMPDFN)'>0
- S CNT=0,SP=""
- S GMPARAM("QUIET")=1
- S GMPARAM("REV")=$P($G(^GMPL(125.99,1,0)),U,5)="R"
- S ORVIEW("ACT")=GMPSTAT
- S ORVIEW("PROV")=0
- S ORVIEW("VIEW")=""
- S ORICD186=$$PATCH^XPDUTL("ICD*18.0*6")
- ;
- D GETPLIST^GMPLMGR1(.ORLIST,.ORTOTAL,.ORVIEW)
- ;
- F NUM=0:0 S NUM=$O(ORLIST(NUM)) Q:NUM'>0  D
- . S IFN=+ORLIST(NUM) Q:IFN'>0
- . S INACT=""
- . S GMPL0=$G(^AUPNPROB(IFN,0))
- . S GMPL1=$G(^AUPNPROB(IFN,1))
- . S HASCMT=($D(^AUPNPROB(IFN,11,0))>0)
- . S CNT=CNT+1
- . I +ORICD186 D
- . . S ICD=$$CODEC^ICDCODE(+GMPL0)
- . . I '+$$STATCHK^ICDAPIU(ICD,DT) S INACT="#"
- . E  D
- . . S ICD=$P($G(^ICD9(+GMPL0,0)),U)
- . S LASTMOD=$P(GMPL0,U,3)
- . S ST=$P(GMPL0,U,12)
- . S ONSET=$P(GMPL0,U,13)
- . S SC=$S(+$P(GMPL1,U,10):"SC",$P(GMPL1,U,10)=0:"NSC",1:"")
- . S AO=$S(+$P(GMPL1,U,11):"/AO",1:"")
- . S IR=$S(+$P(GMPL1,U,12):"/IR",1:"")
- . S ENV=$S(+$P(GMPL1,U,13):"/EC",1:"")
- . S HNC=$S(+$P(GMPL1,U,15):"/HNC",1:"")
- . S MST=$S(+$P(GMPL1,U,16):"/MST",1:"")
- . S CV=$S(+$P(GMPL1,U,17):"/CV",1:"")
- . S SHD=$S(+$P(GMPL1,U,18):"/SHD",1:"")
- . S SCCOND=SC_AO_IR_ENV_HNC_MST_CV_SHD
- . S LOC=$P(GMPL1,U,8)
- . S DTREC=$P(GMPL1,U,9)
- . S LT=""
- . I LOC'="" S LT=$P($G(^SC(LOC,0)),"^",3),LOC=LOC_";"_$P($G(^SC(LOC,0)),U,1)
- . S PROV=$P(GMPL1,U,5) ; responsible provider
- . I PROV'="" S PROV=PROV_";"_$P($G(^VA(200,PROV,0)),U,1)
- . S SERV=$P(GMPL1,U,6)
- . I SERV=0 S SERV="" ; not sure how it gets set to 0, but need consistency in GUI
- . I SERV'="" S SERV=SERV_";"_$P($G(^DIC(49,SERV,0)),U,1)
- . S SP=""
- . F I=11,12,13 S:$P(GMPL1,U,I) SP=SP_$S(I=11:"A",I=12:"I",1:"P")
- . S PRIO=$P(GMPL1,U,14)
- . S LIN=IFN_U_ST_U_$$PROBTEXT^GMPLX(IFN)_U_ICD_U_ONSET
- . S LIN=LIN_U_LASTMOD_U_SC_U_SP_U_$P(GMPL1,U,2)
- . S LIN=LIN_U_LOC_U_LT_U_PROV_U_SERV_U_PRIO_U_HASCMT_U_DTREC_U_SCCOND_U_INACT
- . S GMPL(CNT)=LIN
- S GMPL(0)=CNT
+ S GMPREV=$P($G(^GMPL(125.99,1,0)),U,5)="R"
+ D LIST^GMPLAPI4(.GMPL,GMPDFN,GMPSTAT,"","",GMPREV,0)
  Q
  ;
  ;
@@ -90,60 +39,12 @@ LIST(GMPL,GMPDFN,GMPSTAT)       ; -- Returns list of problems for patient GMPDFN
  ;
 DELLIST(RETURN,GMPDFN) ; GET LIST OF DELETED PROBLEMS
  ; see GETPLIST^GMPLMGR1 and LIST^GMPUTL2
- N S,IFN,I,L0,L1,ST,TXT,ICD,ONSET,MOD,SC,SP,LOC,LT,PROV,SERV,PRIO,HASCMT,DTREC
- N SCCOND,AO,IR,ENV,HNC,MST,CV,SHD,ORICD186,INACT
- S I=0,S=""
- S ORICD186=$$PATCH^XPDUTL("ICD*18.0*6")
- F  S S=$O(^AUPNPROB("ACTIVE",GMPDFN,S)) Q:S=""  D
- . S IFN=""
- . F  S IFN=$O(^AUPNPROB("ACTIVE",+GMPDFN,S,IFN)) Q:IFN=""  D
- .. I $P($G(^AUPNPROB(IFN,1)),U,2)="H" D
- ... S L0=$G(^AUPNPROB(IFN,0))
- ... Q:L0=""
- ... S INACT=""
- ... S L1=$G(^AUPNPROB(IFN,1))
- ... S ST=$P(L0,U,12)
- ... S TXT=$$PROBTEXT^GMPLX(IFN)
- ... I +ORICD186 D
- ... . S ICD=$$CODEC^ICDCODE(+L0)
- ... . I '+$$STATCHK^ICDAPIU(ICD,DT) S INACT="#"
- ... E  D
- ... . S ICD=$P($G(^ICD9(+L0,0)),U)
- ... S ONSET=$P(L0,U,13)
- ... S MOD=$P(L0,U,3)
- ... S SC=$S(+$P(L1,U,10):"SC",$P(L1,U,10)=0:"NSC",1:"")
- ... S AO=$S(+$P(L1,U,11):"/AO",1:"")
- ... S IR=$S(+$P(L1,U,12):"/IR",1:"")
- ... S ENV=$S(+$P(L1,U,13):"/EC",1:"")
- ... S HNC=$S(+$P(L1,U,15):"/HNC",1:"")
- ... S MST=$S(+$P(L1,U,16):"/MST",1:"")
- ... S CV=$S(+$P(L1,U,17):"/CV",1:"")
- ... S SHD=$S(+$P(L1,U,18):"/SHD",1:"")
- ... S SCCOND=SC_AO_IR_ENV_HNC_MST_CV_SHD
- ... S SP=$$GETSP
- ... S LOC=$P(L1,U,8)
- ... S LT=""
- ... I LOC'="" S LT=$P($G(^SC(LOC,0)),"^",3)
- ... S PROV=$P(L1,U,5) ; responsible provider
- ... S SERV=$P(L1,U,6)
- ... S PRIO=$P(L1,U,14)
- ... S HASCMT=($D(^AUPNPROB(IFN,11,0))>0)
- ... S DTREC=$P(L1,U,9)
- ... S I=I+1
- ... S RETURN(I)=IFN_U_ST_U_TXT_U_ICD_U_ONSET
- ... S RETURN(I)=RETURN(I)_U_MOD_U_SC_U_SP_U_$P(L1,U,2)
- ... S RETURN(I)=RETURN(I)_U_LOC_U_LT_U_PROV_U_SERV
- ... S RETURN(I)=RETURN(I)_U_PRIO_U_HASCMT_U_DTREC_U_SCCOND_U_INACT
- S RETURN(0)=I
+ D LIST^GMPLAPI4(.GMPL,GMPDFN,"R","","",0,0)
+ F I=1:1:GMPL D
+ . S $P(GMPL(I),"^",10)=$P($P(GMPL(I),"^",10),";")
+ . S $P(GMPL(I),"^",12)=$P($P(GMPL(I),"^",12),";")
  Q
  ;
-GETSP() ; GET EXPOSURES
- N I
- S SP=""
- F I=11,12,13 S:$P(L1,U,I) SP=SP_$S(I=11:"A",I=12:"I",1:"P")
- Q SP
- ;
- ; adapted from ^GMPLBLD3 ;9/96
  ;
  ; ----------------------- GET USER PROBLEM CATEGORIES --------------
  ;
