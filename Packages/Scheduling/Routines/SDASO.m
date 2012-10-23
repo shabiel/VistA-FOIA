1cfccfd610d390962c80d438d31d1a53a99b3481
vasilelazar <vasile.lazar@infoworld.ro>
2012-10-23 15:21:50 +0300

    Option Append Ancillary Test to Appt, Delete Ancillary Test for Appt.

--- Packages/Scheduling/Routines/SDASO.m
+++ Packages/Scheduling/Routines/SDASO.m
@@ -1,32 +1,42 @@
-SDASO ;MAN/GRR - APPEND TESTS TO PENDING APPOINTMENT ; 22 DEC 83  11:02 am
- ;;5.3;Scheduling;;Aug 13, 1993
+SDASO ;MAN/GRR - APPEND TESTS TO PENDING APPOINTMENT ; 10/23/2012
+ ;;5.3;Scheduling;**260003**;Aug 13, 1993
  S:'$D(DTIME) DTIME=300 D:'$D(DT) DT^SDUTL S HDT=DT,APL=""
-RD S DIC="^DPT(",DIC(0)="AEQM",CNT=0 D ^DIC G:"^"[X END I Y<0 W !,*7,*7,"PATIENT NOT FOUND",*7,*7 G RD
- S DFN=+Y,NAME=$P(Y,"^",2) W ! I $N(^DPT(DFN,"S",HDT))'>0 G NO
- S NDT=HDT,L=0 F J=1:1 S NDT=$N(^DPT(DFN,"S",NDT)) Q:NDT'>0  I $S($P(^(NDT,0),"^",2)']"":1,$P(^(0),"^",2)["I":1,1:0) D CHKSO S SC=+^(0),L=L+1 D FLEN S Z(L)=NDT_"^"_SC_"^"_APL_"^"_COMMENT
- G:L'>0 NO F ZZ=1:1:L W !!,ZZ,") " S Y=$P($P(Z(ZZ),"^",1),".",1) D DT^SDM0 S X=$P(Z(ZZ),"^",1) X ^DD("FUNC",2,1) W " ",$J(X,8)," (",$P(Z(ZZ),"^",3)," MINUTES)  ",$P(^SC($P(Z(ZZ),"^",2),0),"^",1)," ",$P(Z(ZZ),"^",4)
+RD ;
+ N DFN,APTS,SDT,L,CNT
+ S DFN=$$SELPAT^SDMUTL("Patient") G END:DFN<0
+ S %=$$LSTPAPTS^SDMAPI1(.APTS,+DFN,DT,,"ALL")
+ S SDT=0,L=0,CNT=0
+ S NAME=$P(DFN,"^",2) W ! I $O(APTS(""))'>0 G NO
+ F SDT=0:0 S SDT=$O(APTS(SDT)) Q:'SDT  D
+ . S STAT=APTS(SDT,"STATI")
+ . I $S(STAT']"":1,STAT["I":1,1:0) D
+ . . N TST,SEP,COMM S L=L+1,SEP="",TST="",COMM=""
+ . . S:$G(APTS(SDT,"LAB"))>0 TST="LAB",SEP=","
+ . . S:$G(APTS(SDT,"XRAY"))>0 TST=TST_SEP_"XRAY",SEP=","
+ . . S:$G(APTS(SDT,"EKG"))>0 TST=TST_SEP_"EKG",SEP=","
+ . . S:$L(TST)>0 COMM="("_TST_" TEST SCHEDULED)"
+ . . S Z(L)=APTS(SDT,"SD")_U_APTS(SDT,"SC")_U_APTS(SDT,"LEN")_U_COMM
+ . . S Z(L)=Z(L)_U_APTS(SDT,"CLINIC")_U_APTS(SDT,"OE")
+ G:L'>0 NO
+ F ZZ=1:1:L  D
+ . W !!,ZZ,") "
+ . S Y=$P(Z(ZZ),"^",1) D AT^SDUTL S Y=$P(Y,"@",1)_" "_$P(Y,"@",2)
+ . W " ",$J(Y,8)," (",$P(Z(ZZ),"^",3)," MINUTES)  ",$P(Z(ZZ),"^",5)," ",$P(Z(ZZ),"^",4)
 WH R !!,"SCHEDULE TESTS FOR WHICH NUMBERED APPOINTMENT: ",APP:DTIME G:APP=""!(APP="^") RD I APP?."?" D HLP G WH
  I APP'?1N.N W !,"INVALID ENTRY, MUST BE NUMERIC" G WH
  I $L(APP)>5 W !,"ENTER A NUMBER BETWEEN 1 AND ",ZZ G WH
  I APP<1!(APP>ZZ) W !,"ENTER A NUMBER BETWEEN 1 AND ",ZZ G WH
- I $$CO(DFN,+Z(APP),"add") G WH
- S SD=$P(Z(APP),"^",1) S CNT=CNT+1,Y=SD D DTS^SDUTL S SODT=Y,SDWR=0,(LAB,XRAY,EKG)="" D ORD^SDM3 G RD
+ S %=$$ISOECO^SDMAPI4(.ERR,DFN,+Z(APP),"add")
+ I ERR W !,*7 D MSG^DIALOG("WE",,,,"ERR") G WH
+ S SD=$P(Z(APP),"^",1) S CNT=CNT+1,Y=SD D DTS^SDUTL
+ S SODT=Y,SDWR=0 N LAB,XRAY,EKG,ERR
+ D ORDY^SDM3(.LAB,.XRAY,.EKG) G:'$G(LAB)&'$G(XRAY)&'$G(EKG) RD
+ S %=$$ADDTSTS^SDMAPI4(.ERR,+DFN,SD,.LAB,.XRAY,.EKG)
+ I ERR=0 W !,*7 D MSG^DIALOG("WE",,,,"ERR")
+ G RD
 NOPE W:'CNT !,*7,"NOTHING SCHEDULED" G RD
 NO W !,"NO PENDING APPOINTMENTS",*7,*7,*7
  G RD
-FLEN I $D(^SC(SC,"S",NDT)) F ZL=0:0 S ZL=$N(^SC(SC,"S",NDT,1,ZL)) Q:ZL<0  I +^(ZL,0)=DFN S APL=$P(^SC(SC,"S",NDT,1,ZL,0),"^",2) Q
- Q
-CHKSO S COMMENT="",SDAPAV=^(0),SDANAM="LAB"_U_"XRAY"_U_"EKG" F SDJ=3,4,5 I $P(SDAPAV,"^",SDJ)]"" S:$L(COMMENT) COMMENT=COMMENT_"," S COMMENT=COMMENT_$S(SDJ=3:"LAB",SDJ=4:"XRAY",1:"EKG"),@($P(SDANAM,U,SDJ-2))=$P(SDAPAV,U,SDJ)
- ;NAKED REFERENCE - ^DPT(DFN,"S",Date,0)
- S:$L(COMMENT) COMMENT="("_COMMENT_" TEST SCHEDULED)" Q
 END K CNT,NDT,L,J,HDT,SC,SD,APL,COMMENT,Z,ZZ,APP,ZL,SDJ,X,%DT,DIC,DFN,NAME,Y,POP,SDAPAV,SDTY Q
 HLP W !,"Enter the number that corresponds to the appointment." Q
  ;
-CO(DFN,SDT,ACTION) ; -- can action be performed ; has appt been co'ed
- N Y
- S Y=0
- I $P($G(^SCE(+$P(^DPT(DFN,"S",SDT,0),U,20),0)),U,12)=2 D
- .S Y=1
- .W !,*7,"This appointment has been checked out!"
- .W !,"Please use Add/Edit stop code functionality to ",ACTION," the appropriate test."
- Q Y
--- Packages/Scheduling/Routines/SDDSO.m
+++ Packages/Scheduling/Routines/SDDSO.m
@@ -1,27 +1,43 @@
-SDDSO ;BSN/GRR - DELETE ANCILLARY TESTS ;5/8/91  16:23
- ;;5.3;Scheduling;;Aug 13, 1993
- D:'$D(DT) DT^SDUTL S HDT=DT,APL=""
-RD W ! S DIC="^DPT(",DIC(0)="AEQM" D ^DIC G:X=""!(X="^") END I Y<0 W !,*7,*7,"PATIENT NOT FOUND",*7,*7 G RD
- S DA=+Y,DFN=DA,NAME=$P(Y,"^",2) W ! I $N(^DPT(DA,"S",HDT))'>0 G NO
- S NDT=HDT,L=0 F J=1:1 S NDT=$N(^DPT(DA,"S",NDT)) Q:NDT'>0  I $S($P(^(NDT,0),"^",2)']"":1,$P(^(0),"^",2)["I":1,1:0) D CHKSO S SC=+^(0),L=L+1 D FLEN S Z(L)=NDT_"^"_SC_"^"_APL_"^"_COMMENT
-WH1 G:L'>0 NO F ZZ=1:1:L W !!,ZZ,") " S Y=$P($P(Z(ZZ),"^",1),".",1) D DT^SDM0 S X=$P(Z(ZZ),"^",1) X ^DD("FUNC",2,1) W " ",$J(X,8)," (",$P(Z(ZZ),"^",3)," MINUTES)  ",$P(^SC($P(Z(ZZ),"^",2),0),"^",1),"  ",$P(Z(ZZ),"^",4)
+SDDSO ;BSN/GRR - DELETE ANCILLARY TESTS ;10/23/2012
+ ;;5.3;Scheduling;**260003**;Aug 13, 1993
+ S:'$D(DTIME) DTIME=300 D:'$D(DT) DT^SDUTL S HDT=DT,APL=""
+RD ;
+ N DFN,APTS,SDT,L,CNT
+ S DFN=$$SELPAT^SDMUTL("Patient") G END:DFN<0
+ S %=$$LSTPAPTS^SDMAPI1(.APTS,+DFN,DT,,"ALL")
+ S SDT=0,L=0,CNT=0
+ S NAME=$P(DFN,"^",2) W ! I $O(APTS(""))'>0 G NO
+ F SDT=0:0 S SDT=$O(APTS(SDT)) Q:'SDT  D
+ . S STAT=APTS(SDT,"STATI")
+ . I $S(STAT']"":1,STAT["I":1,1:0) D
+ . . N TST,SEP,COMM S L=L+1,SEP="",TST="",COMM=""
+ . . S:$G(APTS(SDT,"LAB"))>0 TST="LAB",SEP=","
+ . . S:$G(APTS(SDT,"XRAY"))>0 TST=TST_SEP_"XRAY",SEP=","
+ . . S:$G(APTS(SDT,"EKG"))>0 TST=TST_SEP_"EKG",SEP=","
+ . . S:$L(TST)>0 COMM="("_TST_" TEST SCHEDULED)"
+ . . S Z(L)=APTS(SDT,"SD")_U_APTS(SDT,"SC")_U_APTS(SDT,"LEN")_U_COMM
+ . . S Z(L)=Z(L)_U_APTS(SDT,"CLINIC")_U_APTS(SDT,"OE")
+WH1 G:L'>0 NO
+ F ZZ=1:1:L  D
+ . W !!,ZZ,") "
+ . S Y=$P(Z(ZZ),"^",1) D AT^SDUTL S Y=$P(Y,"@",1)_" "_$P(Y,"@",2)
+ . W " ",$J(Y,8)," (",$P(Z(ZZ),"^",3)," MINUTES)  ",$P(Z(ZZ),"^",5)," ",$P(Z(ZZ),"^",4)
 WH R !!,"DELETE TEST(S) FOR WHICH NUMBERED APPOINTMENT: ",APP:DTIME G:APP=""!(APP="^") RD G:APP["?" WH1 I APP'?1N.N W !,"INVALID ENTRY, MUST BE NUMERIC" G WH
  I APP<1!(APP>ZZ) W !,"ENTER A NUMBER BETWEEN 1 AND ",ZZ G WH
  S APP=+APP,(SD,S)=$P(Z(APP),"^",1),I=$P(Z(APP),"^",2)
  I Z(APP)'["(" W !,*7,"NO TEST ASSOCIATED WITH THIS APPOINTMENT" G WH1
- I $$CO^SDASO(DFN,S,"delete") G WH1
+ N ERR
+ S %=$$ISOECO^SDMAPI4(.ERR,DFN,S,"delete")
+ I ERR W !,*7 D MSG^DIALOG("WE",,,,"ERR") G WH1
  K LAB,XRAY,EKG
  F ZDT="LAB","XRAY","EKG" D TST
  I '$D(LAB)&('$D(XRAY))&('$D(EKG)) W !,*7,"NOTHING DELETED" G RD
- S SD0=^DPT(DFN,"S",S,0)
- S ^(0)=$P(SD0,"^",1,2)_"^"_$S($D(LAB):"",1:$P(SD0,"^",3))_"^"_$S($D(XRAY):"",1:$P(SD0,"^",4))_"^"_$S($D(EKG):"",1:$P(SD0,"^",5))_"^"_$P(SD0,"^",6,99) G RD ;NAKED REFERENCE - ^DPT(DFN,"S",Date,0)
+ S %=$$DELTSTS^SDMAPI4(.ERR,+DFN,S,.LAB,.XRAY,.EKG)
+ I ERR=0 W !,*7 D MSG^DIALOG("WE",,,,"ERR")
+ G RD
  ;
 NO W !,"NO PENDING APPOINTMENTS",*7,*7,*7
  G RD
-FLEN I $D(^SC(SC,"S",NDT)) F ZL=0:0 S ZL=$N(^SC(SC,"S",NDT,1,ZL)) Q:ZL<0  I +^(ZL,0)=DA S APL=$P(^SC(SC,"S",NDT,1,ZL,0),"^",2) Q
- Q
-CHKSO S COMMENT="" F SDJ=3,4,5 I $P(^(0),"^",SDJ)]"" S:$L(COMMENT) COMMENT=COMMENT_"," S COMMENT=COMMENT_$S(SDJ=3:"LAB",SDJ=4:"XRAY",1:"EKG") ;NAKED REFERENCE - ^DPT(DFN,"S",Date,0)
- S:$L(COMMENT) COMMENT="("_COMMENT_" TEST SCHEDULED)" Q
 TST Q:Z(APP)'[ZDT  S %=1,DTOUT=0 W !,"WANT TO DELETE ",ZDT," TEST" D YN^DICN I '% W !,"RESPOND YES OR NO" G TST
  W:DTOUT " NO" I '(%-1) S @ZDT="" W ?40,"DELETED"
  Q
