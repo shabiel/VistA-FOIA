3bfa90826de0f1a07141d83fa82b6911fa7f1fae
vasilelazar <vasile.lazar@infoworld.ro>
2012-11-12 12:09:46 +0200

    Option Multiple Appointment Booking

--- Packages/Scheduling/Routines/SDMM.m
+++ Packages/Scheduling/Routines/SDMM.m
@@ -1,5 +1,5 @@
-SDMM ;SF/GFT,MAN/GRR - MULTIPLE APPOINTMENTS ; 2/7/05 12:51pm  ; Compiled September 25, 2006 13:33:14
- ;;5.3;Scheduling;**26,32,167,241,327,446**;Aug 13, 1993;Build 77
+SDMM ;SF/GFT,MAN/GRR - MULTIPLE APPOINTMENTS ; 11/12/2012 12:51pm  ; Compiled September 25, 2006 13:33:14
+ ;;5.3;Scheduling;**26,32,167,241,327,446,260003**;Aug 13, 1993;Build 77
  N SDHX,SDAPDT S SDMM=1 D ^SDM K SDMM Q
 RDTY K ^TMP($J,"APPT"),^TMP($J,"SDAMA301") ;SD/327
  R !,"WANT TO MAKE DAILY OR WEEKLY APPOINTMENTS?: WEEKLY// ",SDTYP:DTIME Q:SDTYP["^"!('$T)  S:SDTYP="" SDTYP="W" S SDTYP=$$UP^XLFSTR($E(SDTYP)) I "WD"'[SDTYP W !,"ENTER 'D' FOR DAILY OR PRESS RETURN" G RDTY
@@ -15,11 +15,11 @@ ADT K SDERRFT S CCX=""
  I $E($P(X,"@",2),1,4)?1.4"0" K %DT S X=$P(X,"@"),X=$S($L(X):X,1:"T"),%DT="XF" D ^%DT G ADT:Y'>0 S X1=Y,X2=-1 D C^%DTC S X=X_.24
  K %DT S %DT="TXEF" D ^%DT
  I $P(Y,".",2)=24 S X1=$P(Y,"."),X2=1 D C^%DTC S Y=X_".000001"
- S SDSOH=$S('$D(^SC(+SC,"SL")):0,$P(^("SL"),"^",8)']"":0,1:1)
-OVR S SDY1=$P(Y,".") I $D(^HOLIDAY(SDY1,0)),'SDSOH W *7,?50,$P(^(0),U,2),"??" G ADT
+ S SDSOH=$S($P(CLN("SCHEDULE ON HOLIDAYS?"),"^")']"":0,1:1)
+ N HOL
+OVR S SDY1=$P(Y,".") S %=$$GETHOL^SDMAPI4(.HOL,SDY1) I $D(HOL(0)),'SDSOH W *7,?50,$P(HOL(0),U,2),"??" G ADT
  I $D(SDINA),SDY1'<SDINA,$S('$D(SDRE):1,SDRE>SDY1!('SDRE):1,1:0) S SDY=Y,Y=SDINA D DTS^SDUTL W !,*7,"Clinic is scheduled to be inactivated on ",Y S Y=SDRE D:Y DTS^SDUTL W $S(SDRE:" and reactivated on "_Y,1:"") S Y=SDY K SDY G ADT
  I Y#1=0 G ADT
- D SDFT I $P(Y,".")'<SDEDT W !,*7,"EXCEEDS MAXIMUM DAYS FOR FUTURE APPOINTMENT!!",*7 G ADT
 LEN I $P(SL,U,2)]"" W !,"LENGTH OF APPOINTMENTS (IN MINUTES): ",+SL,"// " R S:DTIME I S]"" G:$L(S)>3 LEN Q:U[S  S POP=0 D L^SDM1 G LEN:POP I S\5*5=S,S'>360,S'<5 S SL=S_U_$P(SL,U,2,99)
  S SDOT=Y#1,SDDAT=$P(Y,"."),X=Y D DOW^SDM0
 RDC W !,"FOR HOW MANY CONSECUTIVE ",$S(SDTYP["W":$P($T(DAY),"^",Y+2)_"DAY'S",1:"DAYS")," DO YOU WANT APPOINTMENTS SCHEDULED",!,"   AT " S X=SDOT D TM W X,"?: "
@@ -30,8 +30,9 @@ OTHER R !,"  OTHER INFO: ",D:DTIME I D["^" W !,*7,"'^' not allowed - hit return 
  I D]"",D?."?"!(D'?.ANP) W "  ENTER LAB, SCAN, ETC." G OTHER
  I $L(D)+$L(SDW)>250 D MSG G OTHER
 BEGIN S SDZM=1,SDZY=Y,SDX9=X,SDM9=D D SDMM^SDM1A K SDZM S Y=SDZY,X=SDX9,D=SDM9
- F SDZ=1:1:SDCN D MAKE^SDMM1 Q:$D(SDERRFT)  D  Q:POP
- .S:SDMADE SDMCNT=SDMCNT+1 I SDMADE,SDZ=1 S SDAPDT=SD
+ S SDT=Y
+ F SDZ=1:1:SDCN S SDMADE=$$MAKE^SDMM1(DFN,SC,Y,SDAPTYP,SDXSCAT,SL,.SDSRTY,2,.D) Q:$D(SDERRFT)  D  Q:POP
+ .S:SDMADE SDMCNT=SDMCNT+1 I SDMADE,SDZ=1 S SDAPDT=SDT
  .S SDMADE=0,POP=0 D GETNEX:SDTYP["W",GETNXD:SDTYP["D"
  .Q
  G:$D(SDERRFT) ADT
@@ -75,7 +76,6 @@ HLP1 W !,"Enter a date/time for the appointment"
  W ".",!,"You may also select 'M' to display the next month's availability or"
  W !,"'D' to specify an earlier or later date to begin the availability display."
  Q
-SDFT S X1=DT,SDEDT=$S($D(^SC(SC,"SDP")):$P(^("SDP"),U,2),1:365) S:'SDEDT SDEDT=365 S X2=SDEDT D C^%DTC S SDEDT=X Q
 MSG W !!?5,"Text entered at OTHER INFO prompt was too long.  Please re-enter.",! Q
  ;
 MORDIS I '$D(SDHX) W *7," ??" G ADT
--- Packages/Scheduling/Routines/SDMM1.m
+++ Packages/Scheduling/Routines/SDMM1.m
@@ -1,70 +1,23 @@
-SDMM1 ;ALB/GRR - MULTIPLE BOOKINGS ; 2/7/05 8:16am
- ;;5.3;Scheduling;**28,206,168,327**;Aug 13, 1993
-MAKE S (SDX3,X,SD)=Y,SM=0 D DOW^SDM0 I $D(^DPT(DFN,"S",X)) S I=^(X,0) I $P(I,"^",2)'["C" W !,"PATIENT ALREADY HAS APPOINTMENT ON ",$P("JAN^FEB^MAR^APR^MAY^JUN^JUL^AUG^SEP^OCT^NOV^DEC","^",$E(X,4,5))," ",$E(X,6,7)," AT THAT TIME" Q
- S SDX7=X D SDFT^SDMM S X=SDX7 I $P(SDX3,".")'<SDEDT W !,*7,"EXCEEDS MAXIMUM DAYS FOR FUTURE APPOINTMENT!!",*7 Q
-S S SDNOT=0 I '$D(^SC(SC,"ST",$P(X,"."),1)) S SS=$O(^SC(+SC,"T"_Y,X)) G X:'SS,X:^(SS,1)="" S ^SC(+SC,"ST",$P(X,"."),1)=$E($P($T(DAY),U,Y+2),1,2)_" "_$E(X,6,7)_$J("",SI+SI-6)_^(1),^(0)=$P(X,".")
-SC S POP=0,SD=X D SC^SDM1 I SDLOCK W ! D DT W " HAS BEEN LOCKED BY ANOTHER USER - APPT NOT BOOKED" L  Q
- G X:POP,OK:SM#9=0 S SDY=Y,Y=X
+SDMM1 ;ALB/GRR - MULTIPLE BOOKINGS ; 11/12/2012 8:16am
+ ;;5.3;Scheduling;**28,206,168,327,260003**;Aug 13, 1993
+MAKE(DFN,SC,SD,TYP,STYP,SL,SRT,LVL,TXT,LAST) ; Make appointmemnt
+ N ERR
+ S %=$$CHKAPP^SDMAPI2(.ERR,SC,DFN,SD,SL,.LVL)
+ I ERR=0 D
+ . S:$P(ERR(0),U)="APTWHEN" $P(ERR(0),U,2)="CLINIC DOES NOT MEET ON "_$$DT(SD)
+ . I $P(ERR(0),U,3)=1 W !!,$P(ERR(0),U,2),! Q
+ . I $P(ERR(0),U,3)>1 D
+ . . S TXT1="...OK"
+ . . I $P(ERR(0),U)="APTPAHA" W !,"PATIENT ALREADY HAS APPOINTMENT ON "_$$DT(SD)_" AT THAT TIME" Q
+ . . S:$P(ERR(0),U)="APTOVBK" $P(ERR(0),U,2)=$$FMTE^XLFDT(SD)_" WILL BE AN OVERBOOK, OK",TXT1=""
+ . . S:$P(ERR(0),U)="APTEXOB" $P(ERR(0),U,2)=$P($P(ERR(0),U,2),","),TXT1=" FOR "_$$FMTE^XLFDT(SD)_", OK"
+ . . S OV=$$OVB^SDM1($P(ERR(0),U,2),TXT1) I OV=2 Q
+ . . I OV=1 S LAST=$$MAKE(DFN,SC,SD,TYP,STYP,SL,.SRT,$P(ERR(0),U,3)-1,.TXT,1)
+ Q:ERR=0 $G(LAST)
+ W !,"APPOINTMENT MADE ON "_$$FMTE^XLFDT(SD)
+ S %=$$MAKE^SDMAPI2(.ERR,DFN,SC,SD,TYP,STYP,.SL,.SRT,.TXT,.CIO,.LAB,.XRAY,.EKG,.RQXRAY,.CNSLTLNK,.LVL)
+ Q 1
  ;
- D OB I SDNOT=0 Q  ; check overbook/keys...quit if not ok
- S SM=9 G SC
- ;
-OK S ^SC(SC,"ST",$P(X,"."),1)=S,^SC(SC,"S",X,0)=X S:'$D(^DPT(DFN,"S",0)) ^(0)="^2.98^^" S:'$D(^SC(SC,"S",0)) ^(0)="^44.001DA^^" L
-S1 L ^SC(SC,"S",X,1):5 G:'$T S1 F Y=1:1 I '$D(^SC(SC,"S",X,1,Y)) S:'$D(^(0)) ^(0)="^44.003PA^^" S ^(Y,0)=DFN_U_(+SL)_U_U_D_U_U_$S($D(DUZ):DUZ,1:"")_U_DT_U_U_U_$S(+SDEMP:+SDEMP,1:"") S SDY=Y L  Q
- I SM S ^("OB")="O" ;NAKED REFERENCE - ^SC(IFN,"S",Date,1,"OB")
- I $D(^SC(SC,"RAD")),^("RAD")="Y"!(^("RAD")=1) S ^SC("ARAD",SC,X,DFN)=""
- S SDINP=$$INP^SDAM2(DFN,X)
- S COV=3,SDYC="",COV=$S(COLLAT=1:1,1:3),SDYC=$S(COLLAT=7:1,1:""),^DPT(DFN,"S",X,0)=SC_"^"_$$STATUS^SDM1A(SC,SDINP,X)_"^^^^^"_COV_"^^^^"_SDYC_"^^^^^"_SDAPTYP_"^^^"_DT_"^^^^^^M^0",SDMADE=1
- D XRDT(DFN,X)  ;xref DATE APPT. MADE field
- K:$D(^DPT("ASDCN",SC,X,DFN)) ^(DFN) K:$D(^DPT(DFN,"S",X,"R")) ^("R")
- S SDRT="A",SDTTM=X,SDPL=SDY,SDSC=SC D RT^SDUTL
- L  W !,"APPOINTMENT MADE ON " S Y=X D DT^DIQ
- ;check for open EWL entries and create TMP($J,"APPT";SD/327
- N SDEV,SD D EN^SDWLEVAL(DFN,.SDEV) S SD=X I SDEV D APPT^SDWLEVAL(DFN,SD,SC)
- D EVT
- Q
- ;
-XRDT(DFN,X) ;cross reference DATE APPT. MADE field
- ;Input: DFN=patient ifn
- ;Input: X=appointment date
- N DIK,DA,DIV S DA=X,DA(1)=DFN
- S DIK="^DPT(DA(1),""S"",",DIK(1)=20 D EN1^DIK
- Q
- ;
-NOOB S SDMES="NO OPEN SLOTS ON "
-WRTER W !,SDMES D DT W:SDNOT " AT THAT TIME" S SDNOT=0 Q
-DT W $P("JAN^FEB^MAR^APR^MAY^JUN^JUL^AUG^SEP^OCT^NOV^DEC","^",$E(X,4,5))," ",$E(X,6,7) Q
-DAY ;;^SUN^MON^TUES^WEDNES^THURS^FRI^SATUR
- ;
-X L  I SDZ=1 W !,*7,"CLINIC DOES NOT MEET THEN!!" S SDERRFT=1 Q
- S SDMES="CLINIC DOES NOT MEET ON " G WRTER
- ;
-EVT ; -- separate tag if need to NEW vars
- N D,SI,SC,SL,COLLAT D MAKE^SDAMEVT(DFN,SDTTM,SDSC,SDPL,0)
- Q
- ;
-OB ; check for overbook keys
- N %,D,I,S,ST
- S SDNOT=1
- I '$D(^XUSEC("SDOB",DUZ)),'$D(^XUSEC("SDMOB",DUZ)) D NOOB G OBQ ; user has neither key
- S I=$P(SD,".",1),(S,ST)=$P(SL,U,7) ; counter of OBs for day = ST
- I ST F D=I-.01:0 S D=$O(^SC(SC,"S",D)) Q:$P(D,".",1)-I  F %=0:0 S %=$O(^SC(SC,"S",D,1,%)) Q:'%  I $P(^(%,0),"^",9)'["C",$D(^("OB")) S ST=ST-1
- I ST<1 D  G OBQ
- . I '$D(^XUSEC("SDMOB",DUZ)) W !,*7,"ONLY "_S_" OVERBOOK"_$E("S",S>1)_" ALLOWED PER DAY!!" D NOOB Q
- . S MXOK=$$DIR("WILL EXCEED MAXIMUM ALLOWABLE OVERBOOKS FOR "_$$FMTE^XLFDT(Y)_", OK","YES")
- . I 'MXOK S SM=9,SDNOT=0 Q
- . I MXOK S S=^SC(SC,"ST",I,1),SM=9,MXOK=""
- I '$D(^XUSEC("SDOB",DUZ)) D NOOB G OBQ
- I '$$DIR($$FMTE^XLFDT(Y)_" WILL BE AN OVERBOOK, OK","NO") S SM=9,SDNOT=0
-OBQ Q
- ;
-DIR(TEXT,DEF) ; reader processor
- ; Input:  TEXT as text of read
- ;         DEF as default response (if any)
- ;
- N DIR,DIROUT,DIRUT,DTOUT,DUOUT,X,Y
- S DIR(0)="Y",DIR("A")=TEXT
- I $G(DEF)]"" S DIR("B")=DEF
- D ^DIR
- W:'Y !
- Q Y
+DT(SD) ;
+ S TXT=$P("JAN^FEB^MAR^APR^MAY^JUN^JUL^AUG^SEP^OCT^NOV^DEC","^",$E(SD,4,5))_" "_$E(SD,6,7)
+ Q TXT
