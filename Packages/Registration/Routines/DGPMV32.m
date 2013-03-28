f0e9654c68f16f4d578562cdc610da9a824b8f87
vasilelazar <vasile.lazar@infoworld.ro>
2013-03-28 14:27:12 +0200

    First commit ADT (REF-141, REF-200)

--- Packages/Registration/Routines/DGPMV32.m
+++ Packages/Registration/Routines/DGPMV32.m
@@ -1,10 +1,10 @@
-DGPMV32 ;ALB/MIR - CONTINUE TRANSFER A PATIENT OPTION ;3/6/2013
+DGPMV32 ;ALB/MIR - CONTINUE TRANSFER A PATIENT OPTION ;3/28/2013
  ;;5.3;Registration;**418,260005**;Aug 13, 1993
  S:$D(PAR("TYPE")) %=$$GETMVTT^DGPMAPI8(.MVTT,PAR("TYPE")),%=$$GETMASMT^DGPMAPI8(.MAS,MVTT("MAS"))
  S DGPMTYP=+$G(MVTT("MAS")) I $S('DGPMTYP:1,'$G(MAS("ABS")):1,1:0) I '$G(PAR("TYPE"))!'$G(PAR("WARD")) W !,"Incomplete Transfer...Deleted" S DGQUIT=1 G Q
  I $S(+$G(PAR("WARD"))_U_+$G(PAR("ROOMBED"))=+$G(OLD("WARD"))_U_+$G(OLD("ROOMBED")):0,'DGPMABL:0,1:1) S DGPMND=DGPMA D AB ;if change in room-bed or ward and next movement is to absence, update subsequent absences
 CONT S DGPMTYP="^"_DGPMTYP_"^" I "^13^44^"[DGPMTYP D ECA^DGPMV321 ;Edit Corresponding admission when TO ASIH or RESUME ASIH
- I DGPMTYP="^43^" D ASIHOF
+ I DGPMTYP="^43^" ;D ASIHOF
  I "^14^45^"[DGPMTYP D UHD^DGPMV321 ;if FROM ASIH or CHANGE ASIH LOCATION (O.F.)
  I 'MVTT("ASKSPEC") G Q
  S Y=0 D SPEC^DGPMV36
@@ -20,15 +20,15 @@ ABB ;absence checks
  S J=$S("^1^23^43^45^"[("^"_$P(DGJJ,"^",18)_"^"):1,1:0),DA=+DGJ,DR=".06////"_$P(DGPMND,"^",6)_$S(J:";.07////"_$P(DGPMND,"^",7),1:"") K DQ,DG
  S ^UTILITY("DGPM",$J,$P(DGJJ,"^",2),DA,"P")=DGJJ D ^DIE S ^UTILITY("DGPM",$J,$P(DGJJ,"^",2),DA,"A")=^DGPM(DA,0)
  Q
-DICS S DGX=+PMVT("TYPE") I $S('$D(^DG(405.1,+DGX,0)):0,'$D(^DG(405.1,+Y,"F",+DGX)):1,1:0) S DGER=1 Q
+DICS S DGX=+$G(PMVT("TYPE")) I $S('$D(^DG(405.1,+DGX,0)):0,'$D(^DG(405.1,+Y,"F",+DGX)):1,1:0) S DGER=1 Q
  S DGX=+$G(NMVT("TYPE")) I $S('$D(^DG(405.1,+DGX,0)):0,'$D(^DG(405.1,+DGX,"F",+Y)):1,1:0) S DGER=1 Q
- S DGX=$P(^DG(405.1,+Y,0),"^",3) I +PMVT("TTYPE")=1,$S('$D(^DG(405.2,+DGX,"E")):0,$P(^("E"),"^",2):0,1:1) S DGER=1 Q
+ S DGX=$P(^DG(405.1,+Y,0),"^",3) I +$G(PMVT("TTYPE"))=1,$S('$D(^DG(405.2,+DGX,"E")):0,$P(^("E"),"^",2):0,1:1) S DGER=1 Q
  I +$G(PMVT("ASIH")),(DGX=14),(+PMVT("MASTYPE")'=45) S DGER=1 Q
  I $D(OLD),"^1^2^3^"[("^"_(+$G(OLD("MASTYPE")))_"^"),(DGX=4) S DGER=1 Q
  ;I "^13^43^44^45"[("^"_DGX_"^"),("^NH^D^"'[("^"_$S($D(^DIC(42,+$P(DGPMAN,"^",6),0)):$P(^(0),"^",3),1:"")_"^")) S DGER=1 Q
  I "^13^43^44^45^"[("^"_DGX_"^"),("^NH^D^"'[("^"_$S($D(^DIC(42,+ADM("WARD"),0)):$P(^(0),"^",3),1:"")_"^"))&($P(^(0),"^",17)'=1) S DGER=1 Q  ;p-418
  ;I DGX=14,("^NH^D^"'[("^"_$S($D(^DIC(42,+$P(DGPMAN,"^",6),0)):$P(^(0),"^",3),1:"")_"^")) S DGER=1 Q
- I DGX=14,("^NH^D^"'[("^"_$S($D(^DIC(42,+ADM("WARD"),0)):$P(^(0),"^",3),1:"")_"^"))&($P(^(0),"^",17)'=1) S DGER=1 Q  ;p-418
+ I DGX=14,("^NH^D^"'[("^"_$S($D(^DIC(42,+$G(ADM("WARD")),0)):$P(^(0),"^",3),1:"")_"^"))&($P(^(0),"^",17)'=1) S DGER=1 Q  ;p-418
  I $G(OLD("ASIH")),(DGX'=+$G(OLD("DISIFN"))) S DGER=1 Q
  I DGX=44,(+$G(NMVT("MASTYPE"))=14) S DGER=1 Q
  S DGER=0 Q
@@ -38,7 +38,7 @@ ASIHOF ;if TO ASIH (OTHER FACILITY) update pseudo discharge
  I $D(^DGPM(+DA,0)) S ^UTILITY("DGPM",$J,3,DA,"P")=$S($D(^UTILITY("DGPM",$J,3,DA,"P")):^("P"),1:^DGPM(DA,0)),DIE="^DGPM(",DR=".01///"_X K DQ,DG D ^DIE S ^UTILITY("DGPM",$J,3,DA,"A")=^DGPM(DA,0) ;update pseudo discharge
  Q
 SET ;set variables if coming from hospital admission (for FINDLAST)
- S DGPMAB=0,DGPMTN=$S($D(^DGPM(+$P(DGPMAN,"^",21),0)):^(0),1:""),DGPMNI=$S($D(^DGPM(+$P(DGPMTN,"^",14),0)):+$P(DGPMTN,"^",14),1:"") Q:'DGPMNI
+ S DGPMAB=0,DGPMTN=$S($D(^DGPM(+$G(ADM("ASIHTRA")),0)):^(0),1:""),DGPMNI=$S($D(^DGPM(+$P(DGPMTN,"^",14),0)):+$P(DGPMTN,"^",14),1:"") Q:'DGPMNI
 FINDLAST ;find the last transfer which originated ASIH care (either a TO ASIH or TO ASIH (OTHER FACILITY) transfer)
  ;
  ;input:  DGPMNI - IFN of NHCU/DOM admission
