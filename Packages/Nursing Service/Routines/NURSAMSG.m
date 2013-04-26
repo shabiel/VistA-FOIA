e77a094fdddb94d8b5d2368ae358cd01280e5aeb
vasilelazar <vasile.lazar@infoworld.ro>
2013-04-26 10:13:52 +0300

    DG: Fixed unexpected ADMIT^DGPMAPI7 message (REF-267)

--- Packages/Nursing Service/Routines/NURSAMSG.m
+++ Packages/Nursing Service/Routines/NURSAMSG.m
@@ -1,5 +1,5 @@
-NURSAMSG ;HIRMFO/RM,FPT-QUEUED MESSAGES TO THE CENT. NURS. OFFICE ;6/18/96  16:27
- ;;4.0;NURSING SERVICE;;Apr 25, 1997
+NURSAMSG ;HIRMFO/RM,FPT-QUEUED MESSAGES TO THE CENT. NURS. OFFICE ;4/26/13
+ ;;4.0;NURSING SERVICE;**260005**;Apr 25, 1997
 EN1 ; CALLED BY THE NURSAWCK AND NURSCPL ROUTINES IF A PATIENT IS NOT 
  ; ADMITTED INTO THE NURSING SERVICE, BUT IS ADMITTED IN MAS
  G BADPARAM:'$D(^DIC(213.9,1,0)),BADPARAM:$P(^DIC(213.9,1,0),"^",2)=""
@@ -15,5 +15,5 @@ NOADM ; THIS MESSAGE SENT TO CNO IF PATIENT NOT ADMITTED AND VALID
  W !,"         and adding the appropriate MAS Location.",!!,"     2.  Then run the option NURS PATIENT File Update (Admit patient)",!,"         (NURSPT-ACT) to admit the patient into the NURSING SERVICE.",@IOF
  Q
 BADPARAM ; ERROR IF NURSING SITE PARAMETERS HAVE NOT BEEN ADDED
- W !!,*7,*7,"Notify NURSING ADP Coordinator and Site Manager that this patient was not",!,"admitted into the NURSING Service because NURSING Site parameters were not",!,"updated."
+ W:'$G(DGQUIET) !!,*7,*7,"Notify NURSING ADP Coordinator and Site Manager that this patient was not",!,"admitted into the NURSING Service because NURSING Site parameters were not",!,"updated."
  Q
